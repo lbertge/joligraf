7830df0dd30bc343aeedf85580bd53a5c16b29f3
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2019-06-23 18:29:53 +0200

    :fire: Keep holomap output + rm duplicate visualiz

--- joligraf/cli/rl/sequence_pair_img_act.py
+++ joligraf/cli/rl/sequence_pair_img_act.py
@@ -20,15 +20,13 @@ hv.extension("bokeh")
 @click.option("--start", type=int, default=0)
 def main(image_folder: str, npz_filepath: str, limit: int, start: int):
     data = np.load(npz_filepath)
-    subfiles_name = list(filter(lambda x: "action" in x, data.files))
-    datasource = {key: data[key] for key in subfiles_name}
+    datasource = {key: data[key] for key in data.files}
     img_folder_path = Path(image_folder)
     images_filepaths = list(img_folder_path.glob("*.png"))
     images_filepaths.sort()
 
     limit = min(start + limit, len(images_filepaths) - 1)
 
-    widgets = []
     with_slides = {}
     for idx, filepath in enumerate(tqdm(images_filepaths[start:limit])):
         dataframe = dict([(key, str(value[idx])) for key, value in datasource.items()])
@@ -37,15 +35,13 @@ def main(image_folder: str, npz_filepath: str, limit: int, start: int):
         image.opts(title=f"Frame #{idx+1}")
         table = hv.Div(pformat(dataframe).replace("\n", "</br>"))
         fig = image + table
-        with_slides[idx] = fig
-        widgets.append(hv.render(fig))
+        with_slides[idx + 1] = fig
 
     data.close()
     hmap = hv.HoloMap(with_slides, "frame")
     hmap = hmap.collate()
 
     hv.save(hmap, "holomap.html")
-    show(column(*widgets))
 
 
 if __name__ == "__main__":
