eb994b4b882150f054fa8be3955e4fb5dcf8e830
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2019-07-06 16:23:40 +0200

    :sparkles: Generic histogram with bokeh

--- joligraf/cli/crossfiles_utils_functions.py
+++ joligraf/cli/crossfiles_utils_functions.py
@@ -2,6 +2,8 @@ from typing import Optional, Tuple
 
 import numpy as np
 from numpy import ndarray
+from bokeh.plotting import figure
+from bokeh.models import HoverTool
 
 
 def scale(
@@ -13,3 +15,22 @@ def scale(
         domain_min, domain_max = in_range
     a, b = out_range
     return a + ((x - domain_min) * (b - a)) / (domain_max - domain_min)
+
+
+def make_histogram(title, hist, edges):
+    p = figure(title=title, background_fill_color="#fafafa")
+    p.quad(
+        top=hist,
+        bottom=0,
+        left=edges[:-1],
+        right=edges[1:],
+        fill_color="navy",
+        line_color="white",
+        alpha=0.5,
+    )
+
+    p.xaxis.axis_label = "x"
+    p.yaxis.axis_label = "Pr(x)"
+    p.grid.grid_line_color = "white"
+    p.add_tools(HoverTool(tooltips=[("count", "@top"), ("bin", "[@left, @right]")]))
+    return p
